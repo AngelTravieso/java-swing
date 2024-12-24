2ba05438024de1a2427335b147ee7a08ef6163c2
AngelTravieso <angeltraviesoc@gmail.com>
2024-12-24 16:28:25 -0400

    JButton y el evento ActionListener

---
+++ src/main/java/Window01Button.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// implements ActionListener
+public class Window01Button extends JFrame implements ActionListener {
+
+    public Window01Button() {
+        super("Ejemplo de Boton y Eventos");
+        var button = new JButton("Aceptar");
+
+        // Con lambda
+        button.addActionListener(event -> System.out.println("Boton pulsado!"));
+        button.addActionListener(this);
+        button.addActionListener(new ButtonClickListener());
+
+        button.setSize(100, 50);
+        getContentPane().add(button);
+        setSize(200, 100);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new Window01Button();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Boton pulsado 2!");
+    }
+
+    private static class ButtonClickListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Boton pulsado 3!");
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }
+}
